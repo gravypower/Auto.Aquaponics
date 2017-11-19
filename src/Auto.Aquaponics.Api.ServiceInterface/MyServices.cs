1f4a3fe44e69ad8250b0b22ae12cde56fb95359d
Aaron Job <aaron@gravypower.net>
2017-11-20 08:17:40 +1100

    added in servicestack

---
+++ src/Auto.Aquaponics.Api.ServiceInterface/MyServices.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using ServiceStack;
+using Auto.Aquaponics.Api.ServiceModel;
+
+namespace Auto.Aquaponics.Api.ServiceInterface
+{
+    public class MyServices : Service
+    {
+        public object Any(Hello request)
+        {
+            return new HelloResponse { Result = "Hello, {0}!".Fmt(request.Name) };
+        }
+    }
+}
---
+++ src/Auto.Aquaponics.Api/AppHost.cs
@@ -0,0 +1,26 @@
+using Funq;
+using Auto.Aquaponics.Api.ServiceInterface;
+using ServiceStack;
+
+namespace Auto.Aquaponics.Api
+{
+    public class AppHost : AppHostBase
+    {
+        /// <summary>
+        /// Base constructor requires a Name and Assembly where web service implementation is located
+        /// </summary>
+        public AppHost()
+            : base("Auto.Aquaponics.Api", typeof(MyServices).GetAssembly()) { }
+
+        /// <summary>
+        /// Application specific configuration
+        /// This method should initialize any IoC resources utilized by your web service classes.
+        /// </summary>
+        public override void Configure(Container container)
+        {
+            //Config examples
+            //this.Plugins.Add(new PostmanFeature());
+            //this.Plugins.Add(new CorsFeature());
+        }
+    }
+}
